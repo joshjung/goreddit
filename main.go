9b3059efebbd18bc457d8d1143b55d24a971bd38
joshjung <joshua.p.jung@gmail.com>
2014-08-23 11:43:23 -0500

    First commit.

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "encoding/json"
+  "fmt"
+)
+
+type Item struct {
+  Title string
+  URL string
+}
+
+type Response struct {
+  Data struct {
+    Children []struct {
+      Data Item
+    }
+  }
+}
+
+func main() {
+  resp, err := http.Get("http://reddit.com/r/golang.json")
+  if err != nil {
+    log.Fatal(err)
+  }
+  if resp.StatusCode != http.StatusOK {
+    log.Fatal(resp.Status)
+  }
+  r := new(Response)
+  err = json.NewDecoder(resp.Body).Decode(r)
+  for _, child := range r.Data.Children {
+    fmt.Println(child.Data.Title)
+  }
+  if err != nil {
+    log.Fatal(err)
+  }
+}
