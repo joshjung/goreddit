7cf8d6adfb69826434a09be04bd3b056294373bd
joshjung <joshua.p.jung@gmail.com>
2014-08-23 12:35:53 -0500

    Phase 2 of tutorial.

--- main.go
+++ main.go
@@ -5,11 +5,26 @@ import (
   "net/http"
   "encoding/json"
   "fmt"
+  "errors"
 )
 
 type Item struct {
   Title string
   URL string
+  Comments int `json:"num_comments"`
+}
+
+func (i Item) String() string {
+  com := ""
+  switch i.Comments {
+    case 0: // Nothing
+  case 1:
+    com = " (1 comment)"
+  default:
+    com = fmt.Sprintf(" (%d comments)", i.Comments)
+  }
+
+  return fmt.Sprintf("%s%s\n%s", i.Title, com, i.URL)
 }
 
 type Response struct {
@@ -20,20 +35,37 @@ type Response struct {
   }
 }
 
-func main() {
-  resp, err := http.Get("http://reddit.com/r/golang.json")
+func Get(reddit string) ([]Item, error) {
+  url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+  resp, err := http.Get(url)
   if err != nil {
-    log.Fatal(err)
+    return nil, err
   }
+  defer resp.Body.Close()
   if resp.StatusCode != http.StatusOK {
-    log.Fatal(resp.Status)
+    return nil, errors.New(resp.Status)
   }
   r := new(Response)
   err = json.NewDecoder(resp.Body).Decode(r)
-  for _, child := range r.Data.Children {
-    fmt.Println(child.Data.Title)
+  if err != nil {
+    return nil, err
   }
+  items := make([]Item, len(r.Data.Children))
+
+  for i, child := range r.Data.Children {
+    items[i] = child.Data
+  }
+
+  return items, nil
+}
+
+func main() {
+  items, err := Get("golang")
   if err != nil {
     log.Fatal(err)
+  }
+
+  for _, item := range items {
+    fmt.Println(item)
   }
 }
